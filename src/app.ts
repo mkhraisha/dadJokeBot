7b81efbb49c44da1cd9c1a3a53282c53eb085483
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-06-07 10:52:17 -0400

    added daily joke

--- src/app.ts
+++ src/app.ts
@@ -23,10 +23,24 @@ export const app = new App({
   receiver: expressReceiver
 })
 
-app.message('hi', async ({ say }) => {
+app.message('hi', async ({ say, message }) => {
   const jokeText = await fetchJoke()
+  console.log(message.channel)
   await say(jokeText.attachments[0].text)
 })
+
+app.command('joke', async ({ say, command }) => {
+  const jokeText = await fetchJoke()
+  console.log(command.channel)
+  await say(jokeText.attachments[0].text)
+})
+
+export const sendDailyJoke = async (): Promise<void> => {
+  app.client.chat.postMessage({
+    channel: '#dadjokebottest',
+    text: (await fetchJoke()).attachments[0].text
+  })
+}
 
 export const fetchJoke = async (): Promise<Joke> => {
   return <Joke>(await axios.get('https://icanhazdadjoke.com/slack')).data
